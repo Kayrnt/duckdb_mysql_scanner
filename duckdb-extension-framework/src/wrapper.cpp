a1bdbd95c33ac5ec13910b3bb6193cbaffb73f0c
Christophe Oudar <kayrnt@gmail.com>
2023-10-25 22:00:20 +0200

    wip

--- duckdb-extension-framework/src/wrapper.cpp
+++ duckdb-extension-framework/src/wrapper.cpp
@@ -21,13 +21,13 @@ auto build_child_list(idx_t n_pairs, const char *const *names, duckdb_logical_ty
 
 extern "C" {
 
-duckdb_logical_type duckdb_create_struct_type(idx_t n_pairs,
-                                              const char **names,
-                                              const duckdb_logical_type *types) {
-  auto *stype = new duckdb::LogicalType;
-  *stype = duckdb::LogicalType::STRUCT(build_child_list(n_pairs, names, types));
-  return reinterpret_cast<duckdb_logical_type>(stype);
-}
+// duckdb_logical_type duckdb_create_struct_type(idx_t n_pairs,
+//                                               const char **names,
+//                                               const duckdb_logical_type *types) {
+//   auto *stype = new duckdb::LogicalType;
+//   *stype = duckdb::LogicalType::STRUCT(build_child_list(n_pairs, names, types));
+//   return reinterpret_cast<duckdb_logical_type>(stype);
+// }
 
 duckdb_logical_type duckdb_create_union(idx_t nmembers,
                                         const char **names,
--- duckdb-extension-framework/src/wrapper.hpp
+++ duckdb-extension-framework/src/wrapper.hpp
@@ -3,8 +3,8 @@
 
 extern "C" {
 
-DUCKDB_EXTENSION_API duckdb_logical_type duckdb_create_struct_type(
-    idx_t n_pairs, const char** names, const duckdb_logical_type* types);
+// DUCKDB_EXTENSION_API duckdb_logical_type duckdb_create_struct_type(
+//     idx_t n_pairs, const char** names, const duckdb_logical_type* types);
 
 DUCKDB_EXTENSION_API duckdb_logical_type duckdb_create_union(
     idx_t nmembers, const char** names, const duckdb_logical_type* types);
